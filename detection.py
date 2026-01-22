c78c15058baecac0f23cb4bda1eacfe4b9833582
Mike <128723321+mikeelprogramador@users.noreply.github.com>
2026-01-22 13:42:56 -0500

    beta 0.0
    En esta versión inicial, se creo el sistema de detección de objetos, guardando dicho objeto en una variable. Se pueden implementar mejoras en el rendimiento, ya que cuando esta en proceso de detección se han notado bajones en el programa, cabe la posibilidad de que sea error de la computadora.

---
+++ detection.py
@@ -0,0 +1,109 @@
+#Librerias necesarias
+import cv2
+from ultralytics import YOLO
+import time
+
+"""
+Documenatcion de la clase Program
+
+Esta clase tiene la funcion en cargada del reconocimiento de dichos objetos atravez del model-face de yolo
+otra funcion que tiene le permite administra la camara por defecto en el computador para que el modelo pueda hacer 
+el reconocimiento de los objetos
+
+"""
+
+class Program:
+    
+    def __init__(self):
+        self.model = YOLO("yolov8n-face.pt") #Se carga el modelo de deteccion de rostros
+        self.key =  True 
+        self.starTime = None
+        self.run()
+        
+        
+    def detectionFace(self, frame):
+        """
+        Documentacion de detectionFace
+        
+        :param self: Description
+        :param frame: cadena visual que se recibe al momneto de ejecutar la camara
+        
+        Este metodo funciona gracias al model-face de yolo8
+        El metod funciona principalmente para detectar unos objetos espeficicos
+        
+        :return fram: Estamos retornando la cadea visual modificada que se recibio como parametro
+        """
+        result = self.model(frame,verbose=False) #"verbose False" desactiva los comit que por defecto, envia ultralytics por consola
+        r = result[0]
+        
+        label = None
+            
+        for box in r.boxes:
+            x1,y1,x2,y2 = map(int, box.xyxy[0]) #Se extraen las cordenadas de la baja
+            cls =  int(box.cls[0])
+            conf = float(box.conf[0]) #Se extrae el umbral como un float
+            
+            if conf < 0.5:
+                continue
+            
+            if conf > 0.8: 
+                label = f"Buscando rostro. {conf:.2f}"#Si se quiere saber la umbral o la confianza
+                color = (0,255,255)
+                
+            if conf >= 0.85:
+                label = f"Rostro detectado. {conf:.2f}" 
+                color = (0,255,0)
+                
+                if self.starTime is None:
+                    self.starTime = time.time()
+                elif time.time() - self.starTime >= 5:#Tiempo de espera para capturar el frame
+                    saveFrame = frame[y1:y2, x1:x2] #Guarda el objeto 
+                    cv2.imwrite("rostro.jpg",saveFrame)
+                    print("frame guardado")
+                    self.key = False
+                    
+        
+        if label:
+            cv2.rectangle(frame, (x1,y1), (x2,y2), color, 2)      #Forma de la caja, con su respectivo color       
+            cv2.putText(frame, label, (x1, y1-5),                    #ubicacion del texto
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)                #tipo de letra con su tamaño y color
+                
+        return frame
+                
+    
+    def run(self):
+        """
+        Documentacion de run
+        
+        :param self: Description
+        
+        Este metodo ejecuta la camara, para dar inicio al reconocimiento de objetos.
+        """
+        #Cargamos la camara por defecto
+        cap = cv2.VideoCapture(0)
+        
+        while(cap.isOpened()):
+            ret,frame = cap.read()
+            
+            if not ret:
+                break
+            elif ret == True:
+                if self.key:
+                    exitFrame = self.detectionFace(frame)
+                else:
+                    exitFrame = frame
+                cv2.imshow('Camare',exitFrame)
+                
+                if cv2.waitKey(1) == 27: #Oprimir la letra ESC para cerrar la ventana
+                    break
+                
+        cap.release()
+        cv2.destroyAllWindows()
+            
+
+            
+            
+# if __name__ == "__main__":
+#     Program()
+
+
---
+++ main.py
@@ -0,0 +1,51 @@
+import detection
+
+"""
+Este proyetco es de Autoria de Santiago Sanchez
+
+El proyecto esta diseñado en pyhton version 3.9
+
+Recomedacion del programador:
+A media de que el proyecto vaya avanzado se ira actualizando
+por el momento para que funcione correctamnete tendras que instalar unas librerias 
+las cuales son:
+
+time
+Ultralytics-yolo 
+Opencv /Version 4.9.0
+pytorch - ("Por el momento esta libreria pyhtorch no se esta utilizando en el proyecto")
+para este ultimo tendran que ingresar a: https://pytorch.org/get-started/previous-versions/
+y ver que version es compatible con su computadora y con python 3.9
+
+"""
+
+print("""
+      Beta 0.0
+      Bienvido al sistema de reconocimiento
+    """)
+    
+while True:
+    opc = 0
+    
+    if opc == 0:
+        print("""
+                Elige una opcion
+                1 - registrarse
+                2 - iniciar sesio
+                3 - salir
+            """)
+    
+    try:
+        opc = int(input())
+    except:
+        print("El dato ingrsado no coincide")
+        
+    if opc == 1:
+        detection.Program()
+        print("Registro finalizado")
+    if opc == 2:
+        print("Esta opcion en desarollo")
+    if opc == 3:
+        print("Gracias por haber visitado el programa, hsata pronto :)")
+        break
+
